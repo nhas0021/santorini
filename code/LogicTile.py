fef2fa2d4f54fc68d1ba42e0434cbffe8cc68053
Naailah Hasan <hasannaailah@gmail.com>
2025-04-28 16:48:21 +0300

    Added type annotation to LogicTile

--- code/LogicTile.py
+++ code/LogicTile.py
@@ -1,7 +1,9 @@
 from MathLib.Vector import Vector2I
+from typing import Optional
+from Worker import Worker
 
 class LogicTile:
     def __init__(self, position: Vector2I):
         self.position = position
         self.stack_height = 0
-        self.worker = None
+        self.worker:Optional[Worker] = None
